23c3bf5f39b0646bd8040ead13994e366ffe42cf
rina0712mmm <rina0712.mmm@gmail.com>
2020-10-23 17:28:16 +0900

    修正箇所編集2

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, except: [:show]
-  before_action :move_to_index, only: [:edit]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show]
 
   def index
     @prototypes = Prototype.all
@@ -50,7 +50,8 @@ class PrototypesController < ApplicationController
   end
 
   def move_to_index
-    unless user_signed_in?
+    @prototype = Prototype.find(params[:id])
+    unless @prototype.user == current_user
       redirect_to action: :index
     end
   end
