9b670b8644724b6635a6204438f6d808da90f113
rina0712mmm <rina0712.mmm@gmail.com>
2020-10-23 11:16:40 +0900

    ユーザーのアクセス範囲の制限

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, except: [:show]
   before_action :move_to_index, only: [:edit]
 
   def index
