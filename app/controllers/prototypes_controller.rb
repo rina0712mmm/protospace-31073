755c13dee730db82767796cc8521d61872899d64
rina0712mmm <rina0712.mmm@gmail.com>
2020-10-26 13:55:08 +0900

    修正箇所編集4

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,15 +1,12 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, except: [:index, :show, :new, :create]
-
+  before_action :authenticate_user!, only: [:new, :create]
+  before_action :move_to_index, only: [:edit]
   def index
     @prototypes = Prototype.all
   end
-
   def new
     @prototype = Prototype.new
   end
-
   def create
     @prototype = Prototype.new(prototype_params)
     if @prototype.save
@@ -18,17 +15,14 @@ class PrototypesController < ApplicationController
       render :new
     end
   end
-
   def show
     @prototype = Prototype.find(params[:id])
     @comment = Comment.new
     @comments = @prototype.comments
   end
-
   def edit
     @prototype = Prototype.find(params[:id])
   end
-
   def update
     prototype = Prototype.find(params[:id])
     if prototype.update(prototype_params)
@@ -37,18 +31,15 @@ class PrototypesController < ApplicationController
       render :edit
     end
   end
-
   def destroy
     prototype = Prototype.find(params[:id])
     prototype.destroy
     redirect_to root_path
   end
-
   private
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
-
   def move_to_index
     @prototype = Prototype.find(params[:id])
     unless @prototype.user == current_user
